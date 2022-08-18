3e8b0f696606afdfec793c9e7895f6f54df74e5f
Lee-moo <dlandif22@gmail.com>
2022-08-18 20:24:55 +0900

    Feat write API about changing job
    기존의 채용 정보를 변경하기 위한 API를 작성했다.

--- routes/job.mjs
+++ routes/job.mjs
@@ -1,9 +1,13 @@
 import express from "express";
-import { enrollJobValidator } from "../validations/jobValidator.mjs";
+import {
+  enrollJobValidator,
+  changeJobValidator,
+} from "../validations/jobValidator.mjs";
 import jobDto from "../dto/jobDto.mjs";
-import { NoExistCompany } from "../userError.mjs";
+import { NoExistCompany, NoExistJob } from "../userError.mjs";
 import { jobRepository, companyRepository } from "../repository/repository.mjs";
 import errorDto from "../dto/errorDto.mjs";
+import jobDtoWithoutCompany from "../dto/jobDtoWithoutCompany.mjs";
 const router = express.Router();
 
 router.post("/", enrollJobValidator(), async (req, res, next) => {
@@ -16,6 +20,20 @@ router.post("/", enrollJobValidator(), async (req, res, next) => {
     if (e instanceof NoExistCompany) {
       return res.status(400).json(errorDto(e.name, e.message));
     }
+    return res.status(400).json(errorDto("", e.message));
+  }
+});
+
+router.patch("/:id", changeJobValidator(), async (req, res, next) => {
+  try {
+    const job = await jobRepository.findJob(req.params.id);
+    await jobRepository.changeJob(job, jobDtoWithoutCompany(req.body));
+    res.status(200).json(jobDtoWithoutCompany(job));
+  } catch (e) {
+    if (e instanceof NoExistJob) {
+      return res.status(400).json(errorDto(e.name, e.message));
+    }
+    return res.status(400).json(errorDto("", e.message));
   }
 });
 
