195bdff4e66a1b31d4d4e62240179a027e92bbe7
Lee-moo <dlandif22@gmail.com>
2022-08-19 12:40:49 +0900

    Feat add DELETE API for deleting a job from user input id
    사용자로부터 입력받은 id로 해당되는 채용공고가 있다면 삭제하는 api

--- routes/job.mjs
+++ routes/job.mjs
@@ -2,6 +2,7 @@ import express from "express";
 import {
   enrollJobValidator,
   changeJobValidator,
+  deleteJobValidator,
 } from "../validations/jobValidator.mjs";
 import jobDto from "../dto/jobDto.mjs";
 import { NoExistCompany, NoExistJob } from "../userError.mjs";
@@ -29,6 +30,19 @@ router.patch("/:id", changeJobValidator(), async (req, res, next) => {
     const job = await jobRepository.findJob(req.params.id);
     await jobRepository.changeJob(job, jobDtoWithoutCompany(req.body));
     res.status(200).json(jobDtoWithoutCompany(job));
+  } catch (e) {
+    if (e instanceof NoExistJob) {
+      return res.status(400).json(errorDto(e.name, e.message));
+    }
+    return res.status(400).json(errorDto("", e.message));
+  }
+});
+
+router.delete("/:id", deleteJobValidator(), async (req, res, next) => {
+  try {
+    const job = await jobRepository.findJob(req.params.id);
+    await jobRepository.deleteJob(job.id);
+    return res.status(200).end();
   } catch (e) {
     if (e instanceof NoExistJob) {
       return res.status(400).json(errorDto(e.name, e.message));
