64d232f0eb12e19eeb3212d00061f19e3ea6ae48
Lee-moo <dlandif22@gmail.com>
2022-08-19 15:35:48 +0900

    Feat write getJob and getCompanyOtherJobs methods
    getJob은 넘겨받은 id 값에 해당하는 채용공고을 찾아서 리턴한다.
    getCompanyOtherJobs는 넘겨받은 job의 company의 다른 채용공고를 반환한다.

--- repository/jobRepository.mjs
+++ repository/jobRepository.mjs
@@ -1,18 +1,63 @@
 import errorMessages from "../errorMessages.mjs";
+import Company from "../models/tables/Company.mjs";
 import Job from "../models/tables/Job.mjs";
 import { NoExistJob } from "../userError.mjs";
+import { Op } from "sequelize";
 
 class JobRepository {
   constructor() {}
+  async getJob(id) {}
   async addJob(job) {}
   async findJob(id) {}
   async changeJob(job, newJob) {}
   async deleteJob(id) {}
+  async getCompanyOtherJobs(company_id, job_id) {}
 }
 
 export class JobRepositoryV1 extends JobRepository {
   constructor() {
     super();
+  }
+  async getJob(id) {
+    try {
+      const job = await Job.findOne({
+        where: { id },
+        include: [
+          {
+            model: Company,
+            required: true,
+          },
+        ],
+      });
+      if (job === null) {
+        throw new NoExistJob("id", errorMessages.NOJOB);
+      }
+      return job;
+    } catch (error) {
+      if (error instanceof NoExistJob) {
+        throw error;
+      }
+      throw new Error("다시 시도해 주세요.");
+    }
+  }
+
+  async getCompanyOtherJobs(company_id, job_id) {
+    try {
+      const jobs = await Job.findAll({
+        attributes: ["id"],
+        where: {
+          company_id,
+          id: {
+            [Op.not]: job_id,
+          },
+        },
+        order: [["createdAt", "DESC"]],
+        limit: 10,
+      });
+      return jobs;
+    } catch (e) {
+      throw new Error("다시 시도해 주세요.");
+    }
   }
 
   async addJob(job) {
@@ -55,10 +100,8 @@ export class JobRepositoryV1 extends JobRepository {
 
   async deleteJob(id) {
     try {
-      console.log(id);
       await Job.destroy({ where: { id } });
     } catch (error) {
-      console.log(error);
       throw new Error("다시 시도해주세요.");
     }
   }
