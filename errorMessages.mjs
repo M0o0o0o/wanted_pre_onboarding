229d9eb1fbbad0cc980e45b17ccb919067df7f7e
Lee-moo <dlandif22@gmail.com>
2022-08-20 20:45:40 +0900

    Feat add error messages
    기존에 중복으로 작성하던 에러 메시지와 채용 공고 지원에 쓰이는 메시지를 추가.

--- errorMessages.mjs
+++ errorMessages.mjs
@@ -2,5 +2,8 @@ export default {
   REQUIRED: "필수 항목입니다.",
   INPUTERROR: "올바른 값을 입력하세요.",
   NOCOMPANY: "등록되지 않은 회사입니다.",
-  NOJOB: "존재하지 않는 채용 공고입니다.",
+  NOJOB: "존재하지 않는 채용공고입니다.",
+  EXISTAPPLY: "이미 지원한 채용공고입니다.",
+  CHECKAPPLYINPUT: "사용자나 채용공고가 존재하지 않습니다.",
+  GENENAL: "다시 시도해 주세요.",
 };
