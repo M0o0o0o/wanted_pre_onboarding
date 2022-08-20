dbc5775e76828ed2e78aa5a17914e50c7547a8bd
Lee-moo <dlandif22@gmail.com>
2022-08-20 18:45:18 +0900

    Feat add getJobs method in jobRepository
    인자로 넘어온 데이터를 토대로 채용공고들을 DB에서 조회하도록 구현했다.
    createdAt를 기준으로 정렬해서 가장 최근의 게시된 채용공고들을 조회하도록 했다.

--- repository/jobRepository.mjs
+++ repository/jobRepository.mjs
@@ -12,6 +12,7 @@ class JobRepository {
   async changeJob(job, newJob) {}
   async deleteJob(id) {}
   async getCompanyOtherJobs(company_id, job_id) {}
+  async getJobs(data) {}
 }
 
 export class JobRepositoryV1 extends JobRepository {
@@ -37,6 +38,34 @@ export class JobRepositoryV1 extends JobRepository {
       if (error instanceof NoExistJob) {
         throw error;
       }
+      throw new Error("다시 시도해 주세요.");
+    }
+  }
+
+  async getJobs(data) {
+    try {
+      const jobs = await Job.findAndCountAll({
+        include: [
+          {
+            model: Company,
+            required: true,
+            where: {
+              name: { [Op.like]: data.company.name },
+              nation: { [Op.like]: data.company.nation },
+              region: { [Op.like]: data.company.region },
+            },
+          },
+        ],
+        where: {
+          duty: { [Op.like]: data.job.duty },
+          preferr: { [Op.like]: data.job.preferr },
+        },
+        order: [["createdAt", "DESC"]],
+        limit: data.cnt,
+        offset: (data.page - 1) * data.cnt,
+      });
+      return jobs;
+    } catch (error) {
       throw new Error("다시 시도해 주세요.");
     }
   }
