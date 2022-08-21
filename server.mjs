27bc825aabc727a9223edaf03f4bbfdabb938ba8
Lee-moo <dlandif22@gmail.com>
2022-08-21 15:46:10 +0900

    Test add app.mjs
    app.mjs에 기존의 server.mjs의 코드를 옮긴 후 server.mjs에서는 listen만 하도록 변경했다.

--- server.mjs
+++ server.mjs
@@ -1,35 +1,7 @@
-import express from "express";
-import dotenv from "dotenv";
-import morgan from "morgan";
-import sequelizeInit from "./models/init.mjs";
-
-import job from "./routes/job.mjs";
-import apply from "./routes/apply.mjs";
-
-dotenv.config();
-
-const app = express();
-
-app.set("port", process.env.PORT || 5050);
-
-sequelizeInit();
-
-app.use(morgan("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.use("/job", job);
-app.use("/apply", apply);
-
-app.use((req, res, next) => {
-  const error = new Error();
-  next(error);
-});
-
-app.use((err, req, res, next) => {
-  res.status(404).send("error occured");
-});
+import app from "./app.mjs";
 
 app.listen(app.get("port"), () => {
   console.log("server on");
 });
+
+export default app;
