989bcfd349294df502a73223ce320434ebd74447
Lee-moo <dlandif22@gmail.com>
2022-08-21 15:49:30 +0900

    Refoctor jobDtoWithoutcompany

--- dto/jobDtoWithoutCompany.mjs
+++ dto/jobDtoWithoutCompany.mjs
@@ -1,8 +1,8 @@
 export default (job) => {
-  return {
-    duty: job.duty,
-    overview: job.overview,
-    preferr: job.preferr,
-    grant: job.grant,
-  };
+  const dto = {};
+  if (job.duty !== undefined) dto.duty = job.duty;
+  if (job.overview !== undefined) dto.overview = job.overview;
+  if (job.preferr !== undefined) dto.preferr = job.preferr;
+  if (job.grant !== undefined) dto.grant = job.grant;
+  return dto;
 };
